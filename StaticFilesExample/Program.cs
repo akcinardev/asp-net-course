331e1c5e3ed6d76661240f7e95a0692c9d269659
akcinar.omer <akcinardev@gmail.com>
2024-11-21 10:30:00 +0100

    static files example

--- StaticFilesExample/Program.cs
+++ StaticFilesExample/Program.cs
@@ -7,7 +7,19 @@ namespace StaticFilesExample
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.Map("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello");
+                });
+            });
+
+            //app.MapGet("/", () => "Hello World!");
 
             app.Run();
         }
