efd344da10f776fbfc5e54e17d5fc92aa03bc55b
akcinar.omer <akcinardev@gmail.com>
2024-11-25 11:47:51 +0100

    controller 2

--- ControllerExample/Controllers/HomeController.cs
+++ ControllerExample/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ControllerExample.Models;
 
 namespace ControllerExample.Controllers
 {
@@ -25,6 +26,44 @@ namespace ControllerExample.Controllers
         public string Contact()
         {
             return "Contact Method";
+        }
+
+        [Route("/person")]
+        public JsonResult Person()
+        {
+            Person person = new Person()
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Ömer",
+                LastName = "Akçınar",
+                Age = 26,
+            };
+            
+            return Json(person);
+            //return new JsonResult(person);
+            //return "{\"key\": \"value\"}";
+        }
+
+        [Route("/virtual-file")]
+        public VirtualFileResult VirtualFile() // Represents a file WITHIN WebRoot (wwwroot) folder
+        {
+            return File("/sample.pdf","application/pdf");
+            //return new VirtualFileResult("/sample.pdf","application/pdf");
+        }
+
+        [Route("/physical-file")]
+        public PhysicalFileResult PhysicalFile() // Represents a file OUTSIDE WebRoot (wwwroot) folder
+        {
+            return PhysicalFile("C:\\Users\\OMRAKC\\source\\repos\\files\\sample.pdf", "application/pdf");
+            //return new PhysicalFileResult("C:\\Users\\OMRAKC\\source\\repos\\files\\sample.pdf", "application/pdf");
+        }
+
+        [Route("/content-file")]
+        public FileContentResult FileContent() // Represents a byte file 
+        {
+            byte[] bytes = System.IO.File.ReadAllBytes("C:\\Users\\OMRAKC\\source\\repos\\files\\sample.pdf");
+            return File(bytes, "application/pdf");
+            //return new FileContentResult(bytes, "application/pdf");
         }
     }
 }
--- ControllerExample/Program.cs
+++ ControllerExample/Program.cs
@@ -9,9 +9,8 @@ namespace ControllerExample
             builder.Services.AddControllers(); // adds all controller classes
 
             var app = builder.Build();
-
+            app.UseStaticFiles();
             app.MapControllers(); // adds routes and maps controllers methods
-
             app.Run();
         }
     }
