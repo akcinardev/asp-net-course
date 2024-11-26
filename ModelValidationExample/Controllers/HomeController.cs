5a05e6e79fa1907e871e45a8f4ee26d2f5869433
akcinar.omer <akcinardev@gmail.com>
2024-11-26 13:15:25 +0100

    model validations - 1

---
+++ ModelValidationExample/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelValidationExample.Models;
+
+namespace ModelValidationExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("register")]
+        public IActionResult Index(Person person)
+        {
+            if(!ModelState.IsValid)
+            {
+                //List<string> errorsList = new List<string>();
+                //List<string> errorList = new List<string>();
+                //foreach(var value in ModelState.Values)
+                //{
+                //    foreach (var error in value.Errors)
+                //    {
+                //        errorList.Add(error.ErrorMessage);
+                //    }
+                //}
+                List<string> errorsList = ModelState.Values.SelectMany(value => value.Errors).Select(err => err.ErrorMessage).ToList();
+                string errors = string.Join("\n", errorsList);
+
+                return BadRequest(errors);
+            }
+
+            return Content($"{person}");
+        }
+    }
+}
---
+++ ModelValidationExample/Models/Person.cs
@@ -0,0 +1,40 @@
+﻿using ModelValidationExample.CustomValidators;
+using System.ComponentModel.DataAnnotations;
+
+namespace ModelValidationExample.Models
+{
+    public class Person
+    {
+        [Required(ErrorMessage = "{0} can not be empty.")]
+        [Display(Name = "Person Name")]
+        [StringLength(40, MinimumLength = 3, ErrorMessage = "{0} should be between {2} and {1} chaaracters long.")]
+        [RegularExpression("^[A-Za-z .]*$", ErrorMessage = "{0} should contain only alphabets, space and dot.")]
+        public string? Name { get; set; }
+
+        [EmailAddress(ErrorMessage = "{0} should be a valid email address.")]
+        [Required(ErrorMessage = "{0} can not be blank.")]
+        public string? Email { get; set; }
+
+        [Phone(ErrorMessage = "{0} should be a valid phone number.")]
+        public string? Phone { get; set; }
+
+        [Required(ErrorMessage = "{0} can not be blank.")]
+        public string? Password { get; set; }
+
+        [Required(ErrorMessage = "{0} can not be blank.")]
+        [Compare("Password",ErrorMessage = "{0} does not match with {1}")]
+        [Display(Name = "Re-entered Password")]
+        public string? ConfirmPassword { get; set; }
+
+        [Range(0, 999.99, ErrorMessage = "{0} should be between {1} and {2}.")]
+        public double? Price { get; set; }
+
+        [MinimumYearValidator(2000)]
+        public DateTime? Birthday { get; set; }
+
+        public override string ToString()
+        {
+            return $"Person Object\nName: {Name}\nEmail: {Email}\nPhone: {Phone}\nPassword: {Password}\nConfirmPassword: {ConfirmPassword}\nPrice: {Price}\nBirthday: {Birthday}";
+        }
+    }
+}
