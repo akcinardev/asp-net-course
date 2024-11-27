bd33a9495591bdeb776510034997a3e2741be9fd
akcinar.omer <akcinardev@gmail.com>
2024-11-27 15:40:46 +0100

    razor - 2

--- RazorViews/Controllers/HomeController.cs
+++ RazorViews/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RazorViews.Models;
 
 namespace RazorViews.Controllers
 {
@@ -8,6 +9,16 @@ namespace RazorViews.Controllers
         [Route("home")]
         public IActionResult Index()
         {
+            ViewData["pageTitle"] = "Razor Views Example";
+
+            List<Person> people = new List<Person>() {
+                new Person() { Name = "Omer", Birthday = Convert.ToDateTime("1998-07-10") , Gender = Gender.Male},
+                new Person() { Name = "Beyza", Birthday = Convert.ToDateTime("1999-02-11") , Gender = Gender.Female},
+                new Person() { Name = "Isra", Birthday = Convert.ToDateTime("1998-06-15") , Gender = Gender.Female}
+            };
+
+            ViewData["people"] = people;
+
             return View();
         }
     }
