37bcb68f13d84b9fa6be9522484ab672a2dfc0c9
akcinar.omer <akcinardev@gmail.com>
2024-11-28 16:13:04 +0100

    dependency injection 1

--- DIExample/Controllers/HomeController.cs
+++ DIExample/Controllers/HomeController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DIExampleServices;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DIExample.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly CitiesService _citiesService;
+
+        // constructor
+        public HomeController()
+        {
+            _citiesService = new CitiesService();
+        }
+
+        [Route("/")]
         public IActionResult Index()
         {
-            return View();
+            List<string> cities = _citiesService.GetCities();
+            return View(cities);
         }
     }
 }
