b8b629d4d358719621f45d63047fb5979555f76e
akcinar.omer <akcinardev@gmail.com>
2024-11-28 10:09:42 +0100

    viewbags and strongly typed views

--- RazorViews/Controllers/HomeController.cs
+++ RazorViews/Controllers/HomeController.cs
@@ -12,14 +12,43 @@ namespace RazorViews.Controllers
             ViewData["pageTitle"] = "Razor Views Example";
 
             List<Person> people = new List<Person>() {
-                new Person() { Name = "Omer", Birthday = Convert.ToDateTime("1998-07-10") , Gender = Gender.Male},
-                new Person() { Name = "Beyza", Birthday = Convert.ToDateTime("1999-02-11") , Gender = Gender.Female},
-                new Person() { Name = "Isra", Birthday = Convert.ToDateTime("1998-06-15") , Gender = Gender.Female}
+                new Person() { Name = "omer", Birthday = Convert.ToDateTime("1998-07-10") , Gender = Gender.Male},
+                new Person() { Name = "jill", Birthday = Convert.ToDateTime("1999-02-02") , Gender = Gender.Female},
+                new Person() { Name = "claire", Birthday = Convert.ToDateTime("1998-01-10") , Gender = Gender.Female}
             };
 
-            ViewData["people"] = people;
+            //ViewData["people"] = people;
+            //ViewBag.people = people;
 
-            return View();
+            return View(people);
+        }
+
+        [Route("details/{name}")]
+        public IActionResult Details(string? name)
+        {
+            if(name == null)
+            {
+                return Content("Person name can not be null");
+            }
+
+            List<Person> people = new List<Person>() {
+                new Person() { Name = "omer", Birthday = Convert.ToDateTime("1998-07-10") , Gender = Gender.Male},
+                new Person() { Name = "jill", Birthday = Convert.ToDateTime("1999-02-02") , Gender = Gender.Female},
+                new Person() { Name = "claire", Birthday = Convert.ToDateTime("1998-01-10") , Gender = Gender.Female}
+            };
+
+            Person matchingPerson = people.Where(person => person.Name == name).FirstOrDefault();
+
+            return View(matchingPerson);
+        }
+
+        [Route("person-product")]
+        public IActionResult PersonWithProduct()
+        {
+            Person person = new Person() { Name = "sara", Birthday = Convert.ToDateTime("1998-03-23"), Gender = Gender.Female };
+            Product product = new Product() { Id = 1, Name = "Laptop" };
+            PersonProductWrapper wrapper = new PersonProductWrapper() { PersonData = person, ProductData = product };
+            return View(wrapper);
         }
     }
 }
---
+++ RazorViews/Models/PersonProductWrapper.cs
@@ -0,0 +1,8 @@
+﻿namespace RazorViews.Models
+{
+    public class PersonProductWrapper
+    {
+        public Person PersonData { get; set; }
+        public Product ProductData { get; set; }
+    }
+}
