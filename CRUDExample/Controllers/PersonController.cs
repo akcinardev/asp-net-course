0c0cf9c419837c0145cff0814f8892f3969852b5
akcinar.omer <akcinardev@gmail.com>
2024-12-10 12:17:57 +0100

    searchview added. search logic implemented

--- CRUDExample/Controllers/PersonController.cs
+++ CRUDExample/Controllers/PersonController.cs
@@ -18,9 +18,23 @@ namespace CRUDExample.Controllers
 
         [Route("/")]
         [Route("/persons/index")]
-        public IActionResult Index()
+        public IActionResult Index(string searchBy, string? searchString)
         {
-            List<PersonResponse> persons = _personService.GetAllPersons();
+            ViewBag.SearchFields = new Dictionary<string, string>()
+            {
+                {nameof(PersonResponse.PersonName), "Person Name" },
+                {nameof(PersonResponse.Email), "Email" },
+                {nameof(PersonResponse.DateOfBirth), "Date of Birth" },
+                {nameof(PersonResponse.Gender), "Gender" },
+                {nameof(PersonResponse.CountryID), "Country" },
+                {nameof(PersonResponse.Address), "Address" },
+            };
+
+            ViewBag.CurrentSearchBy = searchBy;
+            ViewBag.CurrentSearchString = searchString;
+
+            //List<PersonResponse> persons = _personService.GetAllPersons();
+            List<PersonResponse> persons = _personService.GetFilteredPersons(searchBy, searchString);
 
             return View(persons);
         }
